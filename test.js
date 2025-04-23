4608323f3ebf617eb6c309ea695a5d9ef1d8f06f
JING <85792030+daphne6831@users.noreply.github.com>
2025-04-23 09:31:56 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+
+
+//電量顯示按鈕綁點擊事件
+document.querySelector('#now_btn').addEventListener("click",function(){
+    // console.log('test')
+    //插入grafana iframe顯示電量表
+
+    document.querySelector('#result').innerHTML = '<iframe src="http://localhost:3000/d-solo/fejms9z9o4l4wd/mg1-pv?orgId=1&timezone=browser&refresh=auto&theme=light&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>'
+})
