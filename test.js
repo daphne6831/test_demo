0ee70a88eaae023d9f9315dc9b769e6dd699f8b9
JING <85792030+daphne6831@users.noreply.github.com>
2025-04-23 10:57:46 +0800

    change ip

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ document.querySelector('#now_btn').addEventListener("click",function(){
     // console.log('test')
     //插入grafana iframe顯示電量表
 
-        document.querySelector('#result').innerHTML = '<iframe allowfullscreen referrer policy="no-referrer" src="http://localhost:3000/d-solo/fejms9z9o4l4wd/mg1-pv?orgId=1&timezone=browser&refresh=auto&theme=light&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>'
+        document.querySelector('#result').innerHTML = '<iframe allowfullscreen referrer policy="no-referrer" src="http://120.107.180.24:3000/d-solo/fejms9z9o4l4wd/mg1-pv?orgId=1&timezone=browser&refresh=auto&theme=light&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>'
 
 })
