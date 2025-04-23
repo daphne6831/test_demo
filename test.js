0a9731564b568a61c3050c29aabfd704c93c0c1f
JING <85792030+daphne6831@users.noreply.github.com>
2025-04-23 10:22:50 +0800

    Update test.js

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ document.querySelector('#now_btn').addEventListener("click",function(){
     // console.log('test')
     //插入grafana iframe顯示電量表
 
-    document.querySelector('#result').innerHTML = '<iframe src="http://localhost:3000/d-solo/fejms9z9o4l4wd/mg1-pv?orgId=1&timezone=browser&refresh=auto&theme=light&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"     document.querySelector('#result').innerHTML = '<iframe src="http://localhost:3000/d-solo/fejms9z9o4l4wd/mg1-pv?orgId=1&timezone=browser&refresh=auto&theme=light&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>'
-referrer policy="no-referrer"></iframe>'
+        document.querySelector('#result').innerHTML = '<iframe allowfullscreen referrer policy="no-referrer" src="http://localhost:3000/d-solo/fejms9z9o4l4wd/mg1-pv?orgId=1&timezone=browser&refresh=auto&theme=light&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>'
+
 })
